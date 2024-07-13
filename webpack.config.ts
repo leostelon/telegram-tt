30ec9297b13de14ec262fbc016042722b7f5bdad
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2024-07-13 13:29:52 +0530

    chore: added server env

--- webpack.config.ts
+++ webpack.config.ts
@@ -207,6 +207,7 @@ export default function createConfig(
         RELEASE_DATETIME: Date.now(),
         TELEGRAM_API_ID: undefined,
         TELEGRAM_API_HASH: undefined,
+        SERVER_URL: undefined,
         // eslint-disable-next-line no-null/no-null
         TEST_SESSION: null,
         IS_PACKAGED_ELECTRON: false,
